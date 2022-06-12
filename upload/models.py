aae8e479f4b928e721952576b2b8910ac630521f
surajit <devil@DESKTOP-T443TI5.localdomain>
2022-06-12 20:12:58 +0300

    add django fsm

--- upload/models.py
+++ upload/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Upload(models.Model):
+    file_name = models.CharField(max_length=100)
+    file = models.FileField(upload_to="uploads/")
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+
