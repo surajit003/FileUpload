358692747666398d63108b5ca687c9be12a2ac9c
surajit <devil@DESKTOP-T443TI5.localdomain>
2022-06-12 20:19:30 +0300

    add fileupload model

--- upload/admin.py
+++ upload/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from upload import models
+
 # Register your models here.
+
+
+@admin.register(models.Upload)
+class FileUploadAdmin(admin.ModelAdmin):
+    list_display = ("file_name", "uploaded_at", "status", "description")
+    search_fields = ("file_name",)
+    readonly_fields = ("status",)
--- upload/models.py
+++ upload/models.py
@@ -1,10 +1,29 @@
 from django.db import models
+from django_fsm import FSMField
+from django_fsm import transition
 
 
 class Upload(models.Model):
+    STATUS = (
+        ("PENDING", "PENDING"),
+        ("PROCESSING", "PROCESSING"),
+        ("COMPLETED", "COMPLETED"),
+    )
     file_name = models.CharField(max_length=100)
     file = models.FileField(upload_to="uploads/")
     uploaded_at = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
+    status = FSMField(choices=STATUS, default='pending', protected=True)
+
+    def __str__(self):
+        return f"{self.file_name}"
+
+    @transition(field=status, source='pending', target='processing')
+    def change_status_to_processing(self):
+        return "File Status has been changed from PENDING to PROCESSING"
+
+    @transition(field=status, source='processing', target='completed')
+    def change_status_to_completed(self):
+        return "File Status has been changed from PROCESSING to COMPLETED"
+
 
