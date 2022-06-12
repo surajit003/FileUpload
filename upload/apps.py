be8a13ddd3ec7f0e477bfe9fa354d6a5a3dcfa10
surajit <devil@DESKTOP-T443TI5.localdomain>
2022-06-12 21:17:24 +0300

    initial files

--- upload/apps.py
+++ upload/apps.py
@@ -4,4 +4,6 @@ from django.apps import AppConfig
 class UploadConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'upload'
-    from upload.signals import process_file
+
+    def ready(self):
+        from upload.signals import process_file
