6c4e2e2bbd7618679e08c8d4917992d5bce0f796
surajit <devil@DESKTOP-T443TI5.localdomain>
2022-06-12 21:08:27 +0300

    modify task

--- upload/signals.py
+++ upload/signals.py
@@ -1,11 +1,17 @@
+from datetime import timedelta
+
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-
+from redis import Redis
+from rq_scheduler import Scheduler
 from upload.models import Upload
+
+from upload.tasks import parse_file
+
+scheduler = Scheduler(connection=Redis())
 
 
 @receiver(post_save, sender=Upload)
 def process_file(sender, instance, created, **kwargs):
     if created:
-        # do something
-        pass
+        scheduler.enqueue_in(timedelta(minutes=1), parse_file, instance.id)
--- upload/tasks.py
+++ upload/tasks.py
@@ -6,4 +6,6 @@ def parse_file(upload_id):
     Parse the file and return the data
     """
     upload = Upload.objects.get(id=upload_id)
+    upload.change_status_to_processing()
+    upload.save()
 
