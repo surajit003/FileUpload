11912c9f57ac3c583ce3c1b96d80ec0d89fae80c
surajit <devil@DESKTOP-T443TI5.localdomain>
2022-06-13 19:54:05 +0300

    add utf-8 encoding

--- upload/tasks.py
+++ upload/tasks.py
@@ -13,12 +13,12 @@ def parse_file(upload_id):
     upload.change_status_to_processing()
     upload.save()
 
-    with open(upload.file.path, "r") as f:
+    with open(upload.file.path, "r", encoding='utf-8') as f:
         reader = csv.DictReader(f)
         for count, row in enumerate(reader):
             try:
                 if upload.entity == "PRODUCT":
-                    # this can be change to bulk_create or bulk_update
+                    # this can be changed to bulk_create or bulk_update
                     product, _ = Product.objects.update_or_create(
                         sku=row["sku"],
                         defaults={
